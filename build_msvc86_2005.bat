8765439eee398938bfca90621cd931f4ffbc0d21
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-06-21 22:57:45 -0400

    [build_msvc86_2005.bat] Minor improvement to the x86 build tools env init, may change other scripts to follow same format later

--- build_msvc86_2005.bat
+++ build_msvc86_2005.bat
@@ -4,11 +4,13 @@ set OLD_PATH=%PATH%
 set OLD_INCLUDE=%INCLUDE%
 set OLD_LIB=%LIB%
 set OLD_LIBPATH=%LIBPATH%
+set VSINSTALLDIR_HERE=0
 
 IF NOT DEFINED VSINSTALLDIR (
   set VSINSTALLDIR="C:\PROGRA~2\Microsoft Visual Studio 8"
+  set VSINSTALLDIR_HERE=1
 )
-call %VSINSTALLDIR%"\VC\vcvarsall.bat"
+call "%VSINSTALLDIR%\VC\vcvarsall.bat" x86
 
 set build_dir="%cd%\build_msvc86_2005"
 IF EXIST "%build_dir%" rd /s /q "%build_dir%"
@@ -20,5 +22,9 @@ set PATH=%OLD_PATH%
 set INCLUDE=%OLD_INCLUDE%
 set LIB=%OLD_LIB%
 set LIBPATH=%OLD_LIBPATH%
+if VSINSTALLDIR_HERE==1 (
+   set VSINSTALLDIR_HERE=
+   set VSINSTALLDIR=
+)
 
 cd ..
